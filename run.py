2177059abf449643e29cc4865d63c475452162bf
murtovski <murta1991@gmail.com>
2023-05-28 15:31:08 +0000

    Changed repository name and changed the site to be used. Indeed protected by Cloudflare so will use irishjobs.ie

--- run.py
+++ run.py
@@ -3,18 +3,17 @@ from bs4 import BeautifulSoup
 
 
 def extract(page):
-    url = f'https://ie.indeed.com/jobs?q=full+stack+developer&l=Dublin%2C+County+Dublin&start={page}'
+    url = f'https://www.jobs.ie/Jobs.aspx?hd_searchbutton=true&Keywords=Full+Stack+developer&Regions=0&Categories=0&job-search=true'
     header = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
     result = requests.get(url, header)
     doc = BeautifulSoup(result.content, "html.parser")
-    return result.status_code
+    return doc
 
 
 def filter_information(doc):
-    titles = doc.find_all('div', class_='cardOutline')
+    titles = doc.find_all('div', class_='job-details-header')
     return len(titles)
 
 
 info = extract(0)
-print(info)
-#print(filter_information(info))
+print(filter_information(info))
